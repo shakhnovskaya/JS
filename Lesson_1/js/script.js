3bf9b0e0eb88816f614dda3bbf7508b173562da2
Sviatlana <svetik.xomya4ok@gmail.com>
2018-03-29 16:03:17 +0300

    Первый коммит

---
+++ Lesson_1/js/script.js
@@ -0,0 +1,22 @@
+
+var budget = prompt ('Ваш бюджет?');
+var nameShop = prompt ("Название вашего магазина?");
+prompt ("Какой тип товарв будем продавать?");
+prompt ("Какой тип товарв будем продавать?");
+prompt ("Какой тип товарв будем продавать?");
+
+console.log(budget) ;
+console.log(nameShop);
+
+
+mainList = {
+
+	budget,
+	nameShop,
+	shopGoods: [],
+	employers: {},
+	open: true,
+
+}
+
+console.log(mainList);
