9681b8522faecbb1b50e58826dff9c970bb19b88
Sviatlana <svetik.xomya4ok@gmail.com>
2018-03-30 14:37:21 +0300

    Четвертый коммит

--- Lesson_1/js/script.js
+++ Lesson_1/js/script.js
@@ -1,22 +1,20 @@
 
-var budget = prompt ('Ваш бюджет?');
-var nameShop = prompt ("Название вашего магазина?");
-prompt ("Какой тип товарв будем продавать?");
-prompt ("Какой тип товарв будем продавать?");
-prompt ("Какой тип товарв будем продавать?");
+var money = prompt ('Ваш бюджет?');
+var name = prompt ("Название вашего магазина?");
 
-console.log(budget) ;
-console.log(nameShop);
-
-
-mainList = {
-
-	budget,
-	nameShop,
+var mainList = {
+	budget: money,
+	nameShop: name,
 	shopGoods: [],
 	employers: {},
 	open: true,
 
 }
+
+mainList.shopGoods[0] = prompt ("Какой тип товарв будем продавать?");
+mainList.shopGoods[1] = prompt ("Какой тип товарв будем продавать?");
+mainList.shopGoods[2] = prompt ("Какой тип товарв будем продавать?");
+
+alert("бюджет на 1 день:" + mainList.budget/30);
 
 console.log(mainList);
