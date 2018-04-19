077b9aceb7efe6850a5a59ad6c53759b17452455
Sviatlana <svetik.xomya4ok@gmail.com>
2018-04-19 21:06:17 +0300

    ???

--- Diplom/js/script.js
+++ Diplom/js/script.js
@@ -20,4 +20,80 @@ window.addEventListener('DOMContentLoaded', function() {
 	 		 	
 	});
 
+
+
+	let btn = document.getElementById('ready');
+
+	btn.addEventListener('click', function() {
+
+		main.style.display = "block";
+ 		custom.style.display = "none";
+
+		let mainCardsItem = document.getElementsByClassName('main-cards-item'),
+			mainCards = document.querySelector('.main-cards'),
+			candidateBlock = document.getElementsByClassName('candidate-block'),
+				photo = document.getElementsByClassName('photo'),
+				resultCount = document.getElementsByClassName('result-count'),
+				progress = document.getElementsByClassName('progress'),
+				progressBar = document.getElementsByClassName('progress-bar'),
+			name = document.getElementsByClassName('name'),
+			age = document.getElementsByClassName('age'),
+			sex = document.getElementsByClassName('sex'),
+			views = document.getElementsByClassName('views'),
+			bio = document.getElementsByClassName('bio');
+
+		let nameNew = document.getElementById('name'),
+			ageNew = document.getElementById('age'),
+			sexNew = document.getElementById('sex'),
+			viewsNew = document.getElementById('views'),
+			bioNew = document.getElementById('bio');
+
+
+
+
+		let mainCardsItemNew = document.createElement('div');
+			mainCardsItemNew.classList.add('main-cards-item');
+			mainCards.appendChild(mainCardsItemNew);
+
+		let	nameNew = document.createElement('div');
+			nameNew.classList.add('name');
+
+
+		nameNew.addEventListener('change', () => {
+
+			let name_value = nameNew.value;
+			name_value.textContent = mainCardsItemNew.name;
+
+ 
+	});
+
+
+
+		
+
+			// age = document.createElement('div'),
+			// sex = document.createElement('div'),
+			// views = document.createElement('div'),
+			// bio = document.createElement('div');
+
+			
+			// age.classList.add('age');
+			// sex.classList.add('sex');
+			// views.classList.add('views');
+			// bio.classList.add('bio');
+
+		//let name_value = document.getElementById('name');
+			
+
+
+	// name.textContent = name_value.value;
+
+	 
+	});
+
+
+
+	
+
+
 });
